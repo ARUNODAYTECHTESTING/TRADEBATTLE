51c8f05c8824bec209aa81e0a52935b5f2c7a5a0
Saquib <saquib.flutter@gmail.com>
2023-06-03 05:07:24 -0400

    uploadig news image to s3 has been successfully

--- core/settings.py
+++ core/settings.py
@@ -189,9 +189,18 @@ AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID", "")
 AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY", "")
 AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME", "")
 AWS_LOCATION = "static"
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
 
 AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_S3_FILE_OVERWRITE = False
+
+# s3 public media settings
+PUBLIC_MEDIA_LOCATION = 'media'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+DEFAULT_FILE_STORAGE = 'core.storage_backends.PublicMediaStorage'
+AWS_DEFAULT_ACL = 'public-read'
--- news/models.py
+++ news/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from core import storage_backends
 # Create your models here.
 class TimeStamModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -13,6 +13,7 @@ class News(TimeStamModel):
     description = models.TextField()
     published_date = models.DateField()
     pulisher  = models.CharField(max_length=64)
+    image = models.ImageField(upload_to=storage_backends.PublicMediaStorage(),null=True,blank=True)
 
     class Meta:
         ordering = ['-created_at']
