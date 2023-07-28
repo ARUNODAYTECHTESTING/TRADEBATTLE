2cff9283acea20248d914ac580c0d8fa343a85e7
arsalan <arsalank28@gmail.com>
2023-07-28 22:09:13 +0530

    webhook callback added on razoroay and respectly api get called

--- payment/urls.py
+++ payment/urls.py
@@ -1,10 +1,12 @@
 from django.urls.conf import path
 
 from .views import (
-    load_payment_page
+    load_payment_page,
+    PaymentCallBackview
 )
 
 urlpatterns = [
     path("", load_payment_page),
+    path("payment-callback/", PaymentCallBackview.as_view()),
 
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 # Create your views here.
 def load_payment_page(request):
     return render(request, 'payment.html', {})
+
+
+class PaymentCallBackview(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request,*args, **kwargs):
+        try:
+            print("Payment CallBack executed successfully")
+
+        except Exception as e:
+            pass
+
+        finally:
+            pass
