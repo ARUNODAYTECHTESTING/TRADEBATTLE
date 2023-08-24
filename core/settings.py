80d2e40d979fbd1c0f8a6e1e608f2870d0cbc676
Ubuntu <ubuntu@ip-172-26-3-161.ap-south-1.compute.internal>
2023-08-24 16:14:44 +0000

    server settigs file chages

--- core/settings.py
+++ core/settings.py
@@ -181,10 +181,10 @@ SIMPLE_JWT = {
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+else:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
