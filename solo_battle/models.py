21534ae2e88fa1045401f96c629a469745ed00a2
Saquib <saquib.flutter@gmail.com>
2023-09-07 01:10:27 -0400

    models file has modified

--- solo_battle/models.py
+++ solo_battle/models.py
@@ -1,85 +1,109 @@
 from django.db import models
-
+from avtar import models as avtar_models
 BATTLE_STATUS_CHOICES = (
     ('live', 'Live'),
     ('upcoming', 'Upcoming'),
     ('completed', 'Completed'),
 )
 
-class MasterBattle(models.Model):
+class MasterBattle(avtar_models.TmeStampModel):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
+   
+    class Meta:
+        ordering = ("-created_at",)
+
     def __str__(self) -> str:
         return self.name
 
 
-class Battle(models.Model):
+class Battle(avtar_models.TmeStampModel):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    battle_type = models.ForeignKey(MasterBattle, on_delete=models.CASCADE, db_index=True)
+    battle_type = models.ForeignKey(MasterBattle, on_delete=models.CASCADE, db_index=True,related_name='battle')
     battle_status = models.CharField(
-        max_length=10, choices=BATTLE_STATUS_CHOICES
+        max_length=10, choices=BATTLE_STATUS_CHOICES,default='upcoming'
     )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+   
     icon = models.ImageField(upload_to='battles')
     start_at = models.DateTimeField()
     end_at = models.DateTimeField()
-    price = models.IntegerField()
+    price = models.PositiveBigIntegerField()
+    # TODO: added player joins and entry fees
+    player_joined = models.PositiveIntegerField(default=0)
+    entry_fee = models.FloatField(default=0.0)
+
+
+    class Meta:
+        ordering = ("-created_at",)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class QuestionType(avtar_models.TmeStampModel):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("-created_at",)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Question(avtar_models.TmeStampModel):
+    id = models.AutoField(primary_key=True)
+    battle = models.ForeignKey(Battle, on_delete=models.CASCADE,related_name='questions')
+    question_type = models.ForeignKey(QuestionType, on_delete=models.CASCADE,related_name='questions')
+    name = models.CharField(max_length=255)
+   
+    class Meta:
+        ordering = ("-created_at",)
+
+    def __str__(self) -> str:
+        return self.name
     
-    def __str__(self) -> str:
-        return self.name
 
 
-class QuestionType(models.Model):
+class Option(avtar_models.TmeStampModel):
     id = models.AutoField(primary_key=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE,related_name='options')
     name = models.CharField(max_length=255)
+  
+    class Meta:
+        ordering = ("-created_at",)
+
     def __str__(self) -> str:
         return self.name
 
 
-class Question(models.Model):
+class QuestionAttempt(avtar_models.TmeStampModel):
     id = models.AutoField(primary_key=True)
-    battle = models.ForeignKey(Battle, on_delete=models.CASCADE)
-    question_type = models.ForeignKey(QuestionType, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    def __str__(self) -> str:
-        return self.name
-    
-
-
-class Option(models.Model):
-    id = models.AutoField(primary_key=True)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    def __str__(self) -> str:
-        return self.name
-
-
-class QuestionAttempt(models.Model):
-    id = models.AutoField(primary_key=True)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    option = models.ForeignKey(Option, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE,related_name='question_attempts')
+    option = models.ForeignKey(Option, on_delete=models.CASCADE,related_name='question_attempts')
     user = user = models.ForeignKey(
-        "authentication.User", on_delete=models.CASCADE, db_index=True
+        "authentication.User", on_delete=models.CASCADE, db_index=True,related_name='question_attempts'
     )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     total_number_of_entry = models.IntegerField()
     attempt = models.IntegerField()
 
+    class Meta:
+        ordering = ("-created_at",)
 
-class Answer(models.Model):
+    def __str__(self) -> str:
+        return f"{self.question.name} -> {self.option.name}"
+
+class Answer(avtar_models.TmeStampModel):
     id = models.AutoField(primary_key=True)
     user = user = models.ForeignKey(
-        "authentication.User", on_delete=models.CASCADE, db_index=True
+        "authentication.User", on_delete=models.CASCADE, db_index=True,related_name="answers"
     )
-    option = models.ForeignKey(Option, on_delete=models.CASCADE)
+    option = models.ForeignKey(Option, on_delete=models.CASCADE,related_name="answers")
+
+    class Meta:
+        ordering = ("-created_at",)
+
+    def __str__(self) -> str:
+        return f"{self.user.email} -> {self.option.name}"
     
